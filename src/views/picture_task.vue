b493df64882f58945be7d088197e94871a61fbf2
AngelsOf <1020323847@qq.com>
2024-03-22 11:15:39 +0800

    更新

--- src/views/picture_task.vue
+++ src/views/picture_task.vue
@@ -1,194 +1,136 @@
 <template>
-	<div>
-		<div class="container">
-			<div class="search-box">
-				<el-input v-model="query.name" placeholder="用户名" class="search-input mr10" clearable></el-input>
-				<el-button type="primary" :icon="Search" @click="handleSearch">搜索</el-button>
-				<el-button type="warning" :icon="CirclePlusFilled" @click="visible = true">新增</el-button>
-			</div>
-			<el-table :data="tableData" border class="table" ref="multipleTable" header-cell-class-name="table-header">
-				<el-table-column prop="id" label="ID" width="55" align="center"></el-table-column>
-				<el-table-column prop="name" label="用户名" align="center"></el-table-column>
-				<el-table-column label="账户余额" align="center">
-					<template #default="scope">￥{{ scope.row.money }}</template>
-				</el-table-column>
-				<el-table-column label="头像(查看大图)" align="center">
-					<template #default="scope">
-						<el-image
-							class="table-td-thumb"
-							:src="scope.row.thumb"
-							:z-index="10"
-							:preview-src-list="[scope.row.thumb]"
-							preview-teleported
-						>
-						</el-image>
-					</template>
-				</el-table-column>
-				<el-table-column prop="address" label="地址" align="center"></el-table-column>
-				<el-table-column label="账户状态" align="center">
-					<template #default="scope">
-						<el-tag :type="scope.row.state ? 'success' : 'danger'">
-							{{ scope.row.state ? '正常' : '异常' }}
-						</el-tag>
-					</template>
-				</el-table-column>
-
-				<el-table-column prop="date" label="注册时间" align="center"></el-table-column>
-				<el-table-column label="操作" width="280" align="center">
-					<template #default="scope">
-						<el-button type="warning" size="small" :icon="View" @click="handleView(scope.row)">
-							查看
-						</el-button>
-						<el-button
-							type="primary"
-							size="small"
-							:icon="Edit"
-							@click="handleEdit(scope.$index, scope.row)"
-							v-permiss="15"
-						>
-							编辑
-						</el-button>
-						<el-button
-							type="danger"
-							size="small"
-							:icon="Delete"
-							@click="handleDelete(scope.$index)"
-							v-permiss="16"
-						>
-							删除
-						</el-button>
-					</template>
-				</el-table-column>
-			</el-table>
-			<div class="pagination">
-				<el-pagination
-					background
-					layout="total, prev, pager, next"
-					:current-page="query.pageIndex"
-					:page-size="query.pageSize"
-					:total="pageTotal"
-					@current-change="handlePageChange"
-				></el-pagination>
-			</div>
-		</div>
-		<el-dialog
-			:title="idEdit ? '编辑用户' : '新增用户'"
-			v-model="visible"
-			width="500px"
-			destroy-on-close
-			:close-on-click-modal="false"
-			@close="closeDialog"
-		>
-			<TableEdit :data="rowData" :edit="idEdit" :update="updateData" />
-		</el-dialog>
-		<el-dialog title="查看用户详情" v-model="visible1" width="700px" destroy-on-close>
-			<TableDetail :data="rowData" />
-		</el-dialog>
-	</div>
+  <div class="login-wrap">
+    <div class="ms-login">
+      <div class="ms-title">壁画智慧修复全生命管理系统</div>
+      <el-form :model="param" :rules="rules" ref="login" label-width="0px" class="ms-content">
+        <el-form-item prop="username">
+          <el-input v-model="param.username" placeholder="username">
+            <template #prepend>
+              <el-button :icon="User"></el-button>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+              type="password"
+              placeholder="password"
+              v-model="param.password"
+              @keyup.enter="submitForm(login)"
+          >
+            <template #prepend>
+              <el-button :icon="Lock"></el-button>
+            </template>
+          </el-input>
+        </el-form-item>
+        <div class="login-btn">
+          <el-button type="primary" @click="submitForm(login)">登录</el-button>
+        </div>
+        <el-checkbox class="login-tips" v-model="checked" label="记住密码" size="large" />
+        <!-- <p class="login-tips">Tips : 用户密码。</p> -->
+      </el-form>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts" name="basetable">
+<script setup lang="ts">
 import { ref, reactive } from 'vue';
-import { ElMessage, ElMessageBox } from 'element-plus';
-import { Delete, Edit, Search, CirclePlusFilled, View } from '@element-plus/icons-vue';
-import { fetchData } from '../api/index';
-import TableEdit from '../components/table-edit.vue';
-import TableDetail from '../components/table-detail.vue';
+import { useTagsStore } from '../store/tags';
+import { usePermissStore } from '../store/permiss';
+import { useRouter } from 'vue-router';
+import { ElMessage } from 'element-plus';
+import type { FormInstance, FormRules } from 'element-plus';
+import { Lock, User } from '@element-plus/icons-vue';
 
-interface TableItem {
-	id: number;
-	name: string;
-	thumb: string;
-	money: number;
-	state: string;
-	date: string;
-	address: string;
+interface LoginInfo {
+  username: string;
+  password: string;
 }
 
-const query = reactive({
-	address: '',
-	name: '',
-	pageIndex: 1,
-	pageSize: 10
+const lgStr = localStorage.getItem('login-param');
+const defParam = lgStr ? JSON.parse(lgStr) : null;
+const checked = ref(lgStr ? true : false);
+
+const router = useRouter();
+const param = reactive<LoginInfo>({
+  username: defParam ? defParam.username : '',
+  password: defParam ? defParam.password : '',
 });
-const tableData = ref<TableItem[]>([]);
-const pageTotal = ref(0);
-// 获取表格数据
-const getData = async () => {
-	const res = await fetchData();
-	tableData.value = res.data.list;
-	pageTotal.value = res.data.pageTotal || 50;
-};
-getData();
 
-// 查询操作
-const handleSearch = () => {
-	query.pageIndex = 1;
-	getData();
+const rules: FormRules = {
+  username: [
+    {
+      required: true,
+      message: '请输入用户名',
+      trigger: 'blur',
+    },
+  ],
+  password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
 };
-// 分页导航
-const handlePageChange = (val: number) => {
-	query.pageIndex = val;
-	getData();
-};
-
-// 删除操作
-const handleDelete = (index: number) => {
-	// 二次确认删除
-	ElMessageBox.confirm('确定要删除吗？', '提示', {
-		type: 'warning'
-	})
-		.then(() => {
-			ElMessage.success('删除成功');
-			tableData.value.splice(index, 1);
-		})
-		.catch(() => {});
-};
-
-const visible = ref(false);
-let idx: number = -1;
-const idEdit = ref(false);
-const rowData = ref({});
-const handleEdit = (index: number, row: TableItem) => {
-	idx = index;
-	rowData.value = row;
-	idEdit.value = true;
-	visible.value = true;
-};
-const updateData = (row: TableItem) => {
-	idEdit.value ? (tableData.value[idx] = row) : tableData.value.unshift(row);
-	console.log(tableData.value);
-	closeDialog();
+const permiss = usePermissStore();
+const login = ref<FormInstance>();
+const submitForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return;
+  formEl.validate((valid: boolean) => {
+    if (valid) {
+      ElMessage.success('登录成功');
+      localStorage.setItem('ms_username', param.username);
+      const keys = permiss.defaultList[param.username == 'admin' ? 'admin' : 'user'];
+      permiss.handleSet(keys);
+      localStorage.setItem('ms_keys', JSON.stringify(keys));
+      router.push('/');
+      if (checked.value) {
+        localStorage.setItem('login-param', JSON.stringify(param));
+      } else {
+        localStorage.removeItem('login-param');
+      }
+    } else {
+      ElMessage.error('登录失败');
+      return false;
+    }
+  });
 };
 
-const closeDialog = () => {
-	visible.value = false;
-	idEdit.value = false;
-};
-
-const visible1 = ref(false);
-const handleView = (row: TableItem) => {
-	rowData.value = row;
-	visible1.value = true;
-};
+const tags = useTagsStore();
+tags.clearTags();
 </script>
 
 <style scoped>
-.search-box {
-	margin-bottom: 20px;
+.login-wrap {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  background-image: url(../assets/img/login-bg.jpg);
+  background-size: 100%;
 }
-
-.search-input {
-	width: 200px;
+.ms-title {
+  line-height: 50px;
+  text-align: center;
+  font-size: 20px;
+  color: #333;
+  font-weight: bold;
+  padding-top: 10px;
 }
-
-.mr10 {
-	margin-right: 10px;
+.ms-login {
+  width: 350px;
+  border-radius: 5px;
+  background: #fff;
 }
-.table-td-thumb {
-	display: block;
-	margin: auto;
-	width: 40px;
-	height: 40px;
+.ms-content {
+  padding: 10px 30px 30px;
+}
+.login-btn {
+  text-align: center;
+}
+.login-btn button {
+  width: 100%;
+  height: 36px;
+  margin-bottom: 10px;
+}
+.login-tips {
+  font-size: 12px;
+  line-height: 30px;
+  color: #333;
 }
 </style>
